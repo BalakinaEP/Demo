2e504dcfe010f59d86db30010420ad7f3849a7fc
Балакина Екатерина Петровна <114657505+BalakinaEP@users.noreply.github.com>
2025-09-16 14:43:28 +0300

    Create 1.2.c

---
+++ 1.2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * @brief рассчитывает периметр квадрата
+ * @param side сторона квадрата
+ * @return возвращает рассчитанный Периметр
+ */
+double getPerimetr(const double side);
+
+/**
+ * @brief Точка входа в программу
+ * @return возвращает 0, если программма выполнена корректно
+ */
+int main(void)
+{
+    double side;
+    scanf("%lf",&side);
+    printf("Сторона равна %.2lf\n", side);
+    printf("Периметр равен %.2lf",getPerimetr(side));
+
+    return 0;
+}
+
+double getPerimetr(const double side)
+{
+    return side * 4;
+}
